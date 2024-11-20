685eb55f9f126a807cd14baf15c2fde25e63cd24
Anđela Đorđević <andjela1108@gmail.com>
2024-11-20 10:17:08 +0100

    fix: component exam-overview deleted

--- Konteh/front-office-app/src/app/app-routing.module.ts
+++ Konteh/front-office-app/src/app/app-routing.module.ts
@@ -1,10 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
-import { ExamOverviewComponent } from './features/exam-overview/exam-overview.component';
 
 const routes: Routes = [
-  { path: 'exam-overview', component: ExamOverviewComponent },
 ];
 
 @NgModule({
--- Konteh/front-office-app/src/app/app.module.ts
+++ Konteh/front-office-app/src/app/app.module.ts
@@ -4,13 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import { ExamOverviewComponent } from './features/exam-overview/exam-overview.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent,
-    ExamOverviewComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
